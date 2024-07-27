afd71b267550e14885de0512da868b2164891614
action <action@github.com>
2024-07-27 11:11:04 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sat Jul 20 2024 08:52:11 GMT+0000 (Coordinated Universal Time)
+//Sat Jul 27 2024 11:11:04 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 game.import("extension", function (lib, game, ui, get, ai, _status) {
